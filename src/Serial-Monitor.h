bbb5e9ccf810b01e173f8e350ebfc600a9c53384
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-07 18:48:53 +0100

    Update Serial-Monitor.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/Serial-Monitor.h
+++ src/Serial-Monitor.h
@@ -1,4 +1,9 @@
-String monitor_state = "";
+int analogRead0 = analogRead(A0);
+int analogRead1 = analogRead(A1);
+int analogRead2 = analogRead(A2);
+int analogRead3 = analogRead(A3);
+int analogRead4 = analogRead(A4);
+int analogRead5 = analogRead(A5);
 
 class SerialMonitor {
   public:
@@ -7,6 +12,7 @@ class SerialMonitor {
 
 int SerialMonitor::initialize() {
   Serial.begin(9600);
-  Serial.print(monitor_state);
+  Serial.println("A0: " + analogRead0);
+  Serial.println("A1: " + analogRead1);
   return true;
 }
