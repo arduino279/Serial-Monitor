9559d80714d79059f962e2d57812a1b621f5a9c0
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-07 20:31:42 +0100

    Update Serial-Monitor.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/Serial-Monitor.h
+++ src/Serial-Monitor.h
@@ -12,19 +12,13 @@ class SerialMonitor {
 
 int SerialMonitor::initialize() {
   Serial.begin(9600);
-  if(analogRead0 > 0) {
-    Serial.println("Value of Analog Input 0: " + analogRead0);
-  } else if(analogRead1 == null) {
-    Serial.println("Value of Analog Input 1: " + analogRead1);
-  } else if(analogRead2 == null) {
-    Serial.println("Value of Analog Input 2: " + analogRead2);
-  } else if(analogRead3 == null) {
-    Serial.println("A3: " + analogRead3);
-  } else if(analogRead4 == null) {
-    Serial.println("A4: " + analogRead4);
-  } else if(analogRead5 == null) {
-    Serial.println("A5: " + analogRead5);
-  } else {
-  }
   return true;
+}
+
+int SerialMonitor::analogRead0() {
+  if(analogRead0 == NULL) {
+    return NULL;
+  } else {
+    return analogRead0;
+  }
 }
