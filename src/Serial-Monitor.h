6c90baeda1487a500fdb185f7e5087032f221f86
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-12 14:51:23 +0100

    Update Serial-Monitor.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/Serial-Monitor.h
+++ src/Serial-Monitor.h
@@ -18,8 +18,6 @@ int digitalRead_11 = digitalRead(11);
 int digitalRead_12 = digitalRead(12);
 int digitalRead_13 = digitalRead(13);
 
-Serial.begin(9600);
-
 class SerialMonitor {
   public:
     initialize();
@@ -45,127 +43,127 @@ class SerialMonitor {
 
 int SerialMonitor::initialize() {
   value = this->analogRead0();
-  if(value == NULL) {
-    Serial.println("analogRead0 → NULL");
+  if(value == 0) {
+    Serial.println("analogRead0 → 0");
   } else {
     Serial.print("analogRead0 → ");
     Serial.println(value);
   }
   value = this->analogRead1();
-  if(value == NULL) {
-    Serial.println("analogRead1 → NULL");
+  if(value == 0) {
+    Serial.println("analogRead1 → 0");
   } else {
     Serial.print("analogRead1 → ");
     Serial.println(value);
   }
   value = this->analogRead2();
-  if(value == NULL) {
-    Serial.println("analogRead2 → NULL");
+  if(value == 0) {
+    Serial.println("analogRead2 → 0");
   } else {
     Serial.print("analogRead2 → ");
     Serial.println(value);
   }
   value = this->analogRead3();
-  if(value == NULL) {
-    Serial.println("analogRead3 → NULL");
+  if(value == 0) {
+    Serial.println("analogRead3 → 0");
   } else {
     Serial.print("analogRead3 → ");
     Serial.println(value);
   }
   value = this->analogRead4();
-  if(value == NULL) {
-    Serial.println("analogRead4 → NULL");
+  if(value == 0) {
+    Serial.println("analogRead4 → 0");
   } else {
     Serial.print("analogRead4 → ");
     Serial.println(value);
   }
   value = this->analogRead5();
-  if(value == NULL) {
-    Serial.println("analogRead5 → NULL");
+  if(value == 0) {
+    Serial.println("analogRead5 → 0");
   } else {
     Serial.print("analogRead5 → ");
     Serial.println(value);
   }
   value = this->digitalRead2();
-  if(value == NULL) {
-    Serial.println("digitalRead2 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead2 → 0");
   } else {
     Serial.print("digitalRead2 → ");
     Serial.println(value);
   }
   value = this->digitalRead3();
-  if(value == NULL) {
-    Serial.println("digitalRead3 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead3 → 0");
   } else {
     Serial.print("digitalRead3 → ");
     Serial.println(value);
   }
   value = this->digitalRead4();
-  if(value == NULL) {
-    Serial.println("digitalRead4 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead4 → 0");
   } else {
     Serial.print("digitalRead4 → ");
     Serial.println(value);
   }
   value = this->digitalRead5();
-  if(value == NULL) {
-    Serial.println("digitalRead5 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead5 → 0");
   } else {
     Serial.print("digitalRead5 → ");
     Serial.println(value);
   }
   value = this->digitalRead6();
-  if(value == NULL) {
-    Serial.println("digitalRead6 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead6 → 0");
   } else {
     Serial.print("digitalRead6 → ");
     Serial.println(value);
   }
   value = this->digitalRead7();
-  if(value == NULL) {
-    Serial.println("digitalRead7 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead7 → 0");
   } else {
     Serial.print("digitalRead7 → ");
     Serial.println(value);
   }
   value = this->digitalRead8();
-  if(value == NULL) {
-    Serial.println("digitalRead8 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead8 → 0");
   } else {
     Serial.print("digitalRead8 → ");
     Serial.println(value);
   }
   value = this->digitalRead9();
-  if(value == NULL) {
-    Serial.println("digitalRead9 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead9 → 0");
   } else {
     Serial.print("digitalRead9 → ");
     Serial.println(value);
   }
   value = this->digitalRead10();
-  if(value == NULL) {
-    Serial.println("digitalRead10 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead10 → 0");
   } else {
     Serial.print("digitalRead10 → ");
     Serial.println(value);
   }
   value = this->digitalRead11();
-  if(value == NULL) {
-    Serial.println("digitalRead11 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead11 → 0");
   } else {
     Serial.print("digitalRead11 → ");
     Serial.println(value);
   }
   value = this->digitalRead12();
-  if(value == NULL) {
-    Serial.println("digitalRead12 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead12 → 0");
   } else {
     Serial.print("digitalRead12 → ");
     Serial.println(value);
   }
   value = this->digitalRead13();
-  if(value == NULL) {
-    Serial.println("digitalRead13 → NULL");
+  if(value == 0) {
+    Serial.println("digitalRead13 → 0");
   } else {
     Serial.print("digitalRead13 → ");
     Serial.println(value);
@@ -174,144 +172,144 @@ int SerialMonitor::initialize() {
 }
 
 int SerialMonitor::analogRead0() {
-  if(analogRead_0 == NULL) {
-    return NULL;
+  if(analogRead_0 == 0) {
+    return 0;
   } else {
     return analogRead_0;
   }
 }
 
 int SerialMonitor::analogRead1() {
-  if(analogRead_1 == NULL) {
-    return NULL;
+  if(analogRead_1 == 0) {
+    return 0;
   } else {
     return analogRead_1;
   }
 }
 
 int SerialMonitor::analogRead2() {
-  if(analogRead_2 == NULL) {
-    return NULL;
+  if(analogRead_2 == 0) {
+    return 0;
   } else {
     return analogRead_2;
   }
 }
 
 int SerialMonitor::analogRead3() {
-  if(analogRead_3 == NULL) {
-    return NULL;
+  if(analogRead_3 == 0) {
+    return 0;
   } else {
     return analogRead_3;
   }
 }
 
 int SerialMonitor::analogRead4() {
-  if(analogRead_4 == NULL) {
-    return NULL;
+  if(analogRead_4 == 0) {
+    return 0;
   } else {
     return analogRead_4;
   }
 }
 
 int SerialMonitor::analogRead5() {
-  if(analogRead_5 == NULL) {
-    return NULL;
+  if(analogRead_5 == 0) {
+    return 0;
   } else {
     return analogRead_5;
   }
 }
 
 int SerialMonitor::digitalRead2() {
-  if(digitalRead_2 == NULL) {
-    return NULL;
+  if(digitalRead_2 == 0) {
+    return 0;
   } else {
     return digitalRead_2;
   }
 }
 
 int SerialMonitor::digitalRead3() {
-  if(digitalRead_3 == NULL) {
-    return NULL;
+  if(digitalRead_3 == 0) {
+    return 0;
   } else {
     return digitalRead_3;
   }
 }
 
 int SerialMonitor::digitalRead4() {
-  if(digitalRead_4 == NULL) {
-    return NULL;
+  if(digitalRead_4 == 0) {
+    return 0;
   } else {
     return digitalRead_4;
   }
 }
 
 int SerialMonitor::digitalRead5() {
-  if(digitalRead_5 == NULL) {
-    return NULL;
+  if(digitalRead_5 == 0) {
+    return 0;
   } else {
     return digitalRead_5;
   }
 }
 
 int SerialMonitor::digitalRead6() {
-  if(digitalRead_6 == NULL) {
-    return NULL;
+  if(digitalRead_6 == 0) {
+    return 0;
   } else {
     return digitalRead_6;
   }
 }
 
 int SerialMonitor::digitalRead7() {
-  if(digitalRead_7 == NULL) {
-    return NULL;
+  if(digitalRead_7 == 0) {
+    return 0;
   } else {
     return digitalRead_7;
   }
 }
 
 int SerialMonitor::digitalRead8() {
-  if(digitalRead_8 == NULL) {
-    return NULL;
+  if(digitalRead_8 == 0) {
+    return 0;
   } else {
     return digitalRead_8;
   }
 }
 
 int SerialMonitor::digitalRead9() {
-  if(digitalRead_9 == NULL) {
-    return NULL;
+  if(digitalRead_9 == 0) {
+    return 0;
   } else {
     return digitalRead_9;
   }
 }
 
 int SerialMonitor::digitalRead10() {
-  if(digitalRead_10 == NULL) {
-    return NULL;
+  if(digitalRead_10 == 0) {
+    return 0;
   } else {
     return digitalRead_10;
   }
 }
 
 int SerialMonitor::digitalRead11() {
-  if(digitalRead_11 == NULL) {
-    return NULL;
+  if(digitalRead_11 == 0) {
+    return 0;
   } else {
     return digitalRead_11;
   }
 }
 
 int SerialMonitor::digitalRead12() {
-  if(digitalRead_12 == NULL) {
-    return NULL;
+  if(digitalRead_12 == 0) {
+    return 0;
   } else {
     return digitalRead_12;
   }
 }
 
 int SerialMonitor::digitalRead13() {
-  if(digitalRead_13 == NULL) {
-    return NULL;
+  if(digitalRead_13 == 0) {
+    return 0;
   } else {
     return digitalRead_13;
   }
